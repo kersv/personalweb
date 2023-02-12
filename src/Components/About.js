fe8798d911495554b6088adaffe7c79d44aaee4e
kevin xie <simpbum@gmail.com>
2023-02-12 17:46:23 -0500

    added slider/tech stack icon/project desc for project

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,8 +7,18 @@ const About = () => {
         Hello!
       </div>
       <p className="about-me">
-        My name is Kevin Xie, I'm a senior student at Hunter College and expecting to graduate in the Spring of 2024. I like to spend my free time watching movies and listening to music. 
+        My name is <span className="about-me-highlight">Kevin Xie</span>, I'm a <span className="about-me-highlight">senior student</span> at Hunter College and expecting to graduate in the <span className="about-me-highlight">Spring of 2024</span>. I like to spend my free time watching movies and listening to music. 
       </p>
+      <div className="skills-container">
+        <img className="skill-icon" src='https://cdn-icons-png.flaticon.com/512/732/732212.png' alt="icon"></img>
+        <img className="skill-icon" src='https://cdn-icons-png.flaticon.com/512/732/732190.png' alt="icon"></img>
+        <img className="skill-icon" src='https://cdn-icons-png.flaticon.com/512/919/919828.png' alt="icon"></img>
+        <img className="skill-icon" src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' alt="icon"></img>
+        <img className="skill-icon" src='https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png' alt="icon"></img>
+        <img className="skill-icon" src='https://cdn-icons-png.flaticon.com/512/5968/5968358.png' alt="icon"></img>
+        <img className="skill-icon" src='https://cdn-icons-png.flaticon.com/512/5968/5968350.png' alt="icon"></img>
+        <img className="skill-icon" src='https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png' alt="icon"></img>
+      </div>
     </div>
     
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,13 @@
 import React from "react"
 import '../App.css';
-import Image from './Images/images.jpg'
 
 const Home = () => {
   return(
-    <div>
+    <div className="home-container">
       <div className='Home'>
         WELCOME!
       </div>
-      {/* <img src={Image} alt="imageWel"></img> */}
+      <img className='welcome-img' src='https://64.media.tumblr.com/b57877038c3ac101fd90e55e0844ca03/tumblr_ntivsthdRh1tapwsco3_r1_400.gifv' alt="imageWel"></img>
     </div>
   )
 }
